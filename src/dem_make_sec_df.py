84477c81e0bf2b657e84cd72d2e470f8523d03b8
tlohde <ed.holt@ed.ac.uk>
2024-08-22 13:39:07 +0100

    getting sec samples to parquet now works.

--- src/dem_make_sec_df.py
+++ src/dem_make_sec_df.py
@@ -1,5 +1,6 @@
 
 import argparse
+import dask.delayed
 import geopandas as gpd
 import dask.dataframe as da
 import xarray as xr
@@ -7,94 +8,131 @@ from glob import glob
 from tqdm import tqdm
 import rioxarray as rio
 import numpy as np
-
+import dask
+from dask.distributed import LocalCluster, Client
 import os
 from rasterio.enums import Resampling
 from rasterio.features import rasterize
+import odc.geo.xr
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--directory')
 args = parser.parse_args()
 directory = args.directory
 
-def demote_coords_to_vars(ds: xr.Dataset,
-                          coords: str,
-                          var_name: str):
-    '''
-    messy onliner to for reorganizing dataset.
-    e.g. dataset with two variables: a (dims: x, y, t) and b (dims: x, y)
-    this function will convert it to a dataset with 
-    dimensions x, y and add as many `a` variables as there dim `t` is long
-    '''
-    return xr.merge([
-        ds.drop_vars([coords, var_name]),
-        xr.merge(
-            [ds[var_name].isel({coords:i}).rename(ds[coords][i].item())
-             for i in range(len(ds[coords]))], compat='override').drop_vars(coords)]
-                    )
 
-def get_summary_df(site):
-    id = os.path.basename(site).split('_')[0][2:]
-    
-    cl_path = os.path.join(site, glob('*.geojson', root_dir=site)[0])
-    cl = gpd.read_file(cl_path)
-    where = cl.loc[0, 'where']
-    lake_land = cl.loc[0, 'lake_land']
-    
-    sec_path = os.path.join(site, 'sec.zarr')
-    dem_path = os.path.join(site, 'stacked_coregd.zarr')
-    mask_path = os.path.join(site, 'stable_terrain_mask.tif')
-    
-    with rio.open_rasterio(mask_path).squeeze().drop_vars('band') as _mask:
+if __name__ == "__main__":
+
+    cluster = dask.distributed.LocalCluster(n_workers=4,
+                                            threads_per_worker=2,
+                                            memory_limit='8G')
+    client = cluster.get_client()
+    print(client.dashboard_link)
+        
+    def demote_coords_to_vars(ds: xr.Dataset,
+                            coords: str,
+                            var_name: str):
+        '''
+        messy onliner to for reorganizing dataset.
+        e.g. dataset with two variables: a (dims: x, y, t) and b (dims: x, y)
+        this function will convert it to a dataset with 
+        dimensions x, y and add as many `a` variables as there dim `t` is long
+        '''
+        return xr.merge([
+            ds.drop_vars([coords, var_name]),
+            xr.merge(
+                [ds[var_name].isel({coords:i}).rename(ds[coords][i].item())
+                for i in range(len(ds[coords]))], compat='override').drop_vars(coords)]
+                        )
+
+    def add_geom_mask(geom, buffer, ds):
+        # buffer geometry, with square ends
+        buff_geom = geom.buffer(200, cap_style=3)
+        
+        # empty array of same x, y dim shape as merged
+        arr = np.zeros((ds.sizes['y'], ds.sizes['x']))
+        
+        # rasterize
+        burned = rasterize(shapes=[(buff_geom, 1)],
+                        fill=0,
+                        out=arr,
+                        transform=ds.rio.transform())
+        
+        # merged rasterized with all other dataarrays
+        merged = xr.merge([ds, xr.DataArray(data=burned,
+                                            dims=['y','x'],
+                                            coords={'y': ds.y,
+                                                    'x': ds.x}).rename('buffer_aoi')])
+
+        return merged
+
+
+    def get_summary_df(site):
+        id = os.path.basename(site).split('_')[0][2:]
+        
+        cl_path = os.path.join(site, glob('*.geojson', root_dir=site)[0])
+        cl = gpd.read_file(cl_path)
+        where = cl.loc[0, 'where']
+        lake_land = cl.loc[0, 'lake_land']
+        
+        sec_path = os.path.join(site, 'sec.zarr')
+        dem_path = os.path.join(site, 'stacked_coregd.zarr')
+        mask_path = os.path.join(site, 'stable_terrain_mask.tif')
+        
+        _sec = xr.open_zarr(sec_path)
+        _sec = demote_coords_to_vars(_sec, 'result', 'sec')
+        
         with xr.open_zarr(dem_path) as _dem:
-            with xr.open_zarr(sec_path) as _sec:
-                _dem_median = _dem['z'].median(dim='time')
-                _dem_median = _dem_median.rio.reproject_match(_sec, resampling=Resampling.bilinear, nodata=np.nan)
-                mask_rprj = _mask.rio.reproject_match(_sec, resampling=Resampling.bilinear).rename('mask')
-                _sec = demote_coords_to_vars(_sec, 'result', 'sec')
-                # _dem_median_mask = xr.where(mask_rprj == 0, _dem_median, np.nan)
-                # _sec_mask = xr.where(mask_rprj == 0, _sec, np.nan)
-                merged = xr.merge([_dem_median.rename('z_median'), _sec, mask_rprj])
+            _dem_median = _dem['z'].median(dim='time',
+                                        skipna=True).compute()
+            _dem.close()
+        
+        _dem_median = _dem_median.rio.reproject_match(_sec,
+                                                resampling=Resampling.bilinear,
+                                                nodata=np.nan).rename('z_median')
+        
+        
+        # _dem_median = reproject_like(_dem_median, _sec).rename('z_median').compute()
+                    
+        with rio.open_rasterio(mask_path).squeeze().drop_vars('band') as _mask:
+            mask_rprj = _mask.rio.reproject_match(_sec,
+                                                resampling=Resampling.bilinear,
+                                                nodata=0).rename('mask')
 
-                # buffer geometry, with square ends
-                buff_geom = cl.loc[0,'geometry'].buffer(200, cap_style=3)
+            _mask.close()
+        
+        # _dem_median_mask = xr.where(mask_rprj == 0, _dem_median, np.nan)
+        # _sec_mask = xr.where(mask_rprj == 0, _sec, np.nan)
+        merged = xr.merge([_dem_median, _sec, mask_rprj],
+                        compat='override').compute()
+        
+        merged = add_geom_mask(cl.loc[0,'geometry'], 200, merged)
+            
+        ## take logical and of NOT stable terrain and centreline masks,
+        cl_mask = ((merged['mask'] != 1) & (merged['buffer_aoi'] == 1))
 
-                # empty array of same x, y dim shape as merged
-                arr = np.zeros(merged['z_median'].shape)
+        # set everything else to nan
+        cl_masked = xr.where(cl_mask, merged, np.nan)
 
-                # rasterize
-                burned = rasterize(shapes=[(buff_geom, 1)],
-                                fill=0,
-                                out=arr,
-                                transform=merged.rio.transform())
+        # convert to dask dataframe
+        df = (cl_masked
+            .to_dask_dataframe()
+            .dropna()
+            .drop(columns='spatial_ref')
+            .reset_index())
 
-                # merged rasterized with all other dataarrays
-                merged = xr.merge([merged, xr.DataArray(data=burned,
-                                                        dims=['y','x'],
-                                                        coords={'y': merged.y,
-                                                                'x': merged.x}).rename('buffer_aoi')])
-                
-                ## take logical and of stable terrain and centreline masks,
-                cl_mask = ((merged['mask'] == 0) & (merged['buffer_aoi'] == 1))
+        # add additional meta
+        df['where'] = where
+        df['id'] = id
+        df['lake_land'] = lake_land
 
-                # set everything else to nan
-                cl_masked = xr.where(cl_mask, merged, np.nan)
-                
-                # convert to dask dataframe
-                df = (cl_masked
-                      .to_dask_dataframe()
-                      .dropna()
-                      .drop(columns='spatial_ref')
-                      .reset_index())
+        outpath = os.path.join(directory, 'elevation_sample.parquet')
+        print(f'exporting to {outpath}')
+        # return df, outpath
+        da.to_parquet(df=df, path=outpath, compute=True)
+        print('done')
 
-                # add additional meta
-                df['where'] = where
-                df['id'] = id
-                df['lake_land'] = lake_land
-
-                outpath = os.path.join(site, 'elevation_sample.parquet')
-                print(f'exproting to {outpath}')
-                da.to_parquet(df=df, path=outpath, compute=True)
-                print('done')
-                
-get_summary_df(directory)
+    get_summary_df(directory)
+    
+    client.shutdown()
+    cluster.close()
--- src/slurm_get_sec_df.sh
+++ src/slurm_get_sec_df.sh
@@ -1,10 +1,100 @@
 #!/usr/bin/env bash
 
-#SBATCH --ntasks=16
+#SBATCH --ntasks=8
 #SBATCH --mem=32G
 
 source /opt/conda/etc/profile.d/conda.sh
 conda activate /home/s1759665/micromamba/envs/paper2
 
-/usr/bin/env time python src/dem_make_sec_df.py --directory $1
-echo "done"
+printf "\nworking on id0_\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id0_-138892x_-3197992y
+
+printf "\nworking on id10\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id10_499410x_-1351353y
+
+printf "\nworking on id11\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id11_497441x_-1338620y
+
+printf "\nworking on id12\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id12_-581577x_-1263468y
+
+printf "\nworking on id13\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id13_143649x_-865563y
+
+printf "\nworking on id14\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id14_-239437x_-2838869y
+
+printf "\nworking on id15\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id15_4515x_-3211902y
+
+printf "\nworking on id1_\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id1_6685x_-3188046y
+
+printf "\nworking on id16\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id16_-9318x_-3152179y
+
+printf "\nworking on id17\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id17_-167858x_-3142959y
+
+printf "\nworking on id18\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id18_-280014x_-2902289y
+
+printf "\nworking on id19\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id19_-263550x_-2709771y
+
+printf "\nworking on id20\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id20_-225808x_-2493072y
+
+printf "\nworking on id21\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id21_288157x_-930867y
+
+printf "\nworking on id2_\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id2_-194085x_-3077800y
+
+printf "\nworking on id22\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id22_-281294x_-1899575y
+
+printf "\nworking on id23\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id23_1378285x_-2468304y
+
+printf "\nworking on id24\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id24_1380703x_-2459835y
+
+printf "\nworking on id25\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id25_1383119x_-2447526y
+
+printf "\nworking on id26\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id26_1365033x_-2493323y
+
+printf "\nworking on id27\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id27_1364036x_-2507363y
+
+printf "\nworking on id28\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id28_1365106x_-2513662y
+
+printf "\nworking on id29\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id29_1368245x_-2521726y
+
+printf "\nworking on id30\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id30_1386295x_-2438214y
+
+printf "\nworking on id3_\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id3_-254432x_-3020649y
+
+printf "\nworking on id4_\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id4_-229209x_-2972450y
+
+printf "\nworking on id5_\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id5_-226289x_-2968847y
+
+printf "\nworking on id6_\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id6_-246487x_-2871885y
+
+printf "\nworking on id7_\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id7_-289260x_-1889815y
+
+printf "\nworking on id8_\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id8_607229x_-1847482y
+
+printf "\nworking on id9_\n"
+/usr/bin/env time python src/dem_make_sec_df.py --directory data/id9_484789x_-1406074y
